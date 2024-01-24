d94c0e697488f812ae04124df28ac6df39cbc880
t-coffe <tcoffe@edenschool.fr>
2024-01-24 10:19:04 +0100

    function caret not showing when input.value.length less than 1 is done

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 import './style.css'
 import './second.css'
 import './tablette.css'
+
+const inputName = document.getElementById('name');
+const inputEmail = document.getElementById('email');
+const formButton = document.querySelector('button');
+
+function changeCaret(input) {
+    if (input.value.length > 0) {
+        input.style.caretColor = "#d9d9d9"
+    }else {
+        input.style.caretColor = "#d9d9d900"
+    }
+}
+
+inputName.addEventListener('input', ()=> {
+    changeCaret(inputName);
+})
