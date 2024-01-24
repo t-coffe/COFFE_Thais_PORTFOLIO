92768e75b9a86fd39aa7a560e8d59955735a3e1f
t-coffe <tcoffe@edenschool.fr>
2024-01-24 10:38:59 +0100

    wip javascript form conditions

--- main.js
+++ main.js
@@ -6,14 +6,30 @@ const inputName = document.getElementById('name');
 const inputEmail = document.getElementById('email');
 const formButton = document.querySelector('button');
 
+const regex1 = /[0-9]/g;
+
 function changeCaret(input) {
     if (input.value.length > 0) {
-        input.style.caretColor = "#d9d9d9"
+        input.style.caretColor = "#d9d9d9";
     }else {
-        input.style.caretColor = "#d9d9d900"
+        input.style.caretColor = "#d9d9d900";
     }
 }
 
 inputName.addEventListener('input', ()=> {
     changeCaret(inputName);
+    if (inputName.value.length > 5 && inputName.value.search(" ")>-1 && inputName.value.search(regex1)<0) {
+        inputName.style.borderBottomColor = "#4ee1a0" ;
+    } else {
+        inputName.style.borderBottomColor = "#ff6f5b";
+    }
+})
+
+inputEmail.addEventListener('input', ()=>{
+    changeCaret(inputEmail);
+    if () {
+        
+    } else {
+        
+    }
 })
